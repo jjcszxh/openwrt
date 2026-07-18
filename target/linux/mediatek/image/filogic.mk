019f5b8c6566f92ac24899dee17c67c74d068ea0
jjcszxh <openwrt@OpenWrtcompilation.com>
2026-07-18 22:47:51 +0800

    mediatek: add BeeconMini SEED AC1 support

--- target/linux/mediatek/image/filogic.mk
+++ target/linux/mediatek/image/filogic.mk
@@ -866,6 +866,16 @@ define Device/buffalo_wsr-6000ax8
 endef
 TARGET_DEVICES += buffalo_wsr-6000ax8
 
+define Device/beeconmini_seed-ac1
+  DEVICE_VENDOR := BeeconMini
+  DEVICE_MODEL := SEED AC1
+  DEVICE_DTS := mt7981b-beeconmini-seed-ac1
+  DEVICE_DTS_DIR := ../dts
+  DEVICE_PACKAGES := -wpad-basic-mbedtls kmod-i2c-gpio kmod-sfp kmod-usb3 kmod-fs-f2fs mkf2fs
+  IMAGE/sysupgrade.bin := sysupgrade-tar | append-metadata
+endef
+TARGET_DEVICES += beeconmini_seed-ac1
+
 define Device/cetron_ct3003
   DEVICE_VENDOR := Cetron
   DEVICE_MODEL := CT3003
