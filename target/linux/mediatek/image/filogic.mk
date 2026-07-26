518331cc20218cff081ac9e45942930ee6df288b
jjcszxh <openwrt@OpenWrtcompilation.com>
2026-07-26 23:27:14 +0800

    mediatek: update filogic image configuration

--- target/linux/mediatek/image/filogic.mk
+++ target/linux/mediatek/image/filogic.mk
@@ -865,7 +865,7 @@ define Device/beeconmini_seed-ac1
   DEVICE_MODEL := SEED AC1
   DEVICE_DTS := mt7981b-beeconmini-seed-ac1
   DEVICE_DTS_DIR := ../dts
-  DEVICE_PACKAGES := -wpad-basic-mbedtls kmod-i2c-gpio kmod-sfp kmod-usb3 kmod-fs-f2fs mkf2fs
+  DEVICE_PACKAGES := -wpad-basic-mbedtls kmod-i2c-gpio kmod-sfp kmod-usb3 kmod-fs-f2fs mkf2fs kmod-fs-ext4  e2fsprogs kmod-mt7981-firmware mt7981-wo-firmware
   IMAGE/sysupgrade.bin := sysupgrade-tar | append-metadata
 endef
 TARGET_DEVICES += beeconmini_seed-ac1
